1ef1175b15bcfa9c6fa61903283ed0a7890fcf12
foxfurry <isacartur@gmail.com>
2022-06-19 10:42:54 +0300

    Cors fix

--- internal/http/server/server.go
+++ internal/http/server/server.go
@@ -78,7 +78,7 @@ func (s *server) jwtMiddleware(c *gin.Context) {
 func (s *server) corsMiddleware(c *gin.Context) {
 	c.Header("Access-Control-Allow-Origin", "*")
 	c.Header("Access-Control-Allow-Credentials", "true")
-	c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+	c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With, test_uuid")
 	c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH, OPTIONS, GET, PUT")
 
 	if c.Request.Method == "OPTIONS" {
